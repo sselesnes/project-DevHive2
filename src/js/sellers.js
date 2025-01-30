1531a32f7b9ab81ee9a18737b40142596e182ad5
Yuri Boblak <160606809+sselesnes@users.noreply.github.com>
2025-01-30 08:17:42 +0200

    fixed modal height

--- src/js/sellers.js
+++ src/js/sellers.js
@@ -11,3 +11,13 @@ document
     document.body.classList.remove('modal-open');
     document.querySelector('.top-sellers-modal').classList.remove('is-open');
   });
+
+function updateModalHeight() {
+  document.documentElement.style.setProperty(
+    '--modal-height',
+    `${window.innerHeight * 0.9}px`
+  );
+}
+
+window.addEventListener('resize', updateModalHeight);
+window.addEventListener('load', updateModalHeight);
